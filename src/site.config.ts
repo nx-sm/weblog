aee53744c237f85a88537b75249df7aa7c3b736b
Sergio Matsak <sergio.matsak@hotmail.com>
2023-03-06 00:23:33 +0000

    Configure site and header info

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,10 +1,10 @@
 const siteMeta = {
-	author: "Chris Williams",
-	title: "Astro Theme Cactus",
-	description: "An opinionated starter theme for Astro",
+	author: "Sergiy Matsak",
+	title: "Sergiy Matsak",
+	description: "A journey in understanding the intersection of AI and Cognitive Neuroscience.",
 	lang: "en-GB",
 	ogLocale: "en_GB",
-	githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+	githubUrl: "https://github.com/nx-sm",
 	themeColorLight: "#fafafa",
 	themeColorDark: "#1d1f21",
 };
